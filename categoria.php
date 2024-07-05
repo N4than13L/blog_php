5114f1b084eb60b5fa6fdceb3dfd8dbc63d53aed
Nathaniel <natharevolution@gmail.com>
2024-07-05 10:51:57 -0400

    corrigiendo error de ortografia de las entradas

--- categoria.php
+++ categoria.php
@@ -24,7 +24,7 @@ if (!isset($categoria_actual['id'])) {
                     <h2>
                         <?= $entrada['titulo']; ?>
                     </h2>
-                    <span class="fecha"><?= $entrada['categoria'] . ' ' . $entrada['fecha']; ?></span>
+                    <span class="fecha"><?= $entrada['categorias'] . ' ' . $entrada['fecha']; ?></span>
                     <p>
                         <?= substr($entrada['descripcion'], 0, 200) . "..."; ?>
                     </p>
